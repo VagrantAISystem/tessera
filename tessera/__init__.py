5f511b786c54a414747388623ceaf002f772aac7
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-25 16:37:32 -0400

    app registers api blueprint now

--- tessera/__init__.py
+++ tessera/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from tessera.lib.models import *
 
-import sqlalchemy as sa
-from tessera import config
+import config
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -12,13 +12,10 @@ db = SQLAlchemy(app)
 # Error handlers
 @app.errorhandler(404)
 def not_found(error):
-    return "This is not the page you were looking for. *FORCE SOUNDS*"
+    return "This is not the JSON you were looking for. *FORCE SOUNDS*"
 
 # Register blueprints
-# from tessera.users.controller import users
-
-# app.register_blueprints(users)
-# app.register_blueprints(sessions)
-# app.register_blueprints(teams)
+from tessera.api import bp
+app.register_blueprints(bp)
 
 db.create_all()
