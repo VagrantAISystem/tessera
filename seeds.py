6a849cde7ecacc5596c15d4ffe25d66a00c53384
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-25 16:37:25 -0400

    fixed issue where team_lead wasn't set

--- seeds.py
+++ seeds.py
@@ -7,6 +7,7 @@ testadmin = User("testadmin", "test@example.com", "test", "Test Testerson II")
 test = User("test", "test1@example.com", "test", "Test Testerson")
 db.session.add(testadmin)
 db.session.add(test)
+db.session.commit()
 
 a_team = Team("The A Team")
 a_team.team_lead = test
@@ -17,9 +18,11 @@ testp = Project("TEST", "Test Project")
 testp.project_lead = testadmin
 a_team.projects.append(testp)
 
-for i in range(1, 100):
-    t = Ticket(testp.pkey + str(i), "This is test ticket #" + str(i), 
-               "This isn't helplful")
+for i in range(100):
+    t = Ticket(testp.pkey + "-" +
+               str(Project.query.filter_by(pkey=testp.pkey).first().tickets.count() + 1),
+        "This is test ticket #" + str(i + 1), "This isn't helplful")
+
     testp.tickets.append(t)
 
 db.session.add(a_team)
