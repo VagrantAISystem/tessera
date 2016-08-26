a67d78ec9be5481f96a1fb435bd9c52961e1f323
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-26 15:54:19 -0400

    added a global cache

--- tessera/__init__.py
+++ tessera/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.contrib.cache import RedisCache
 
 import config
 
@@ -7,15 +8,27 @@ app = Flask(__name__)
 app.config.from_object(config)
 
 db = SQLAlchemy(app)
+try:
+    cache = RedisCache(host=config.REDIS_HOST, port=config.REDIS_PORT, 
+                       password=None,db=0, default_timeout=300, key_prefix=None)
+except:
+    # If redis isn't set up default to a simple in memory cache.
+    # This is bad for performance but makes testing and development a little
+    # easier
+    from werkzeug.contrib.cache import SimpleCache
+    cache = SimpleCache()
 
 # Error handlers
 @app.errorhandler(404)
 def not_found(error):
-    return "This is not the JSON you were looking for. *FORCE SOUNDS*"
+    return jsonify({ 
+        "errorMsg": "This is not the JSON you're looking for. *FORCE SOUNDS*",
+    })
 
 # Register blueprints
 from tessera.api import API
 app.register_blueprint(API)
 
+# Create the database
 import tessera.lib.models
 db.create_all()
