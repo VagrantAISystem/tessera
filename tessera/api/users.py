c072cffd7a35a833a7c5922f054c2bb0b88a722e
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-26 15:54:14 -0400

    renamed, no need to have a seperate module

--- tessera/api/users.py
+++ tessera/api/users.py
@@ -1,8 +1,10 @@
 from tessera.lib.models import User
+from tessera.lib.tokens import auth_required
 from flask import jsonify
 from tessera.api import API
 
 @API.route("/users", methods=["GET"])
+@auth_required
 def index():
     users = [ u.serialize() for u in User.query.all() ]
     print(users)
