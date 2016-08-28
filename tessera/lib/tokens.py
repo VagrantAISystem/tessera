80a95ce19afc0df47824d089e7a77483f868127c
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-28 11:30:04 -0400

    fixed issues with auth_required decorator

--- tessera/lib/tokens.py
+++ tessera/lib/tokens.py
@@ -1,23 +1,27 @@
 import config
 import jwt
-import datetime as dt
+
+from datetime import timedelta
+from datetime import datetime as dt
+
+from tessera import app
 
 from functools import wraps
-from flask import g
+from flask import g, request, jsonify
 
 
 def create_token(sub):
     payload = {
         'sub': sub,
         'iat': dt.utcnow(),
-        'exp': dt.utcnow() + dt.timedelta(days=1)
+        'exp': dt.utcnow() + timedelta(days=1)
     }
 
     token = jwt.encode(payload, config.SECRET_KEY, algorithm='HS256')
     return token.decode('unicode_escape')
 
 def parse_token(req):
-    token = req.headers.get('Authorization').split()[1]
+    token = req.headers.get('Authorization')
     return jwt.decode(token, config.SECRET_KEY, algorithms=['HS256'])
 
 def auth_required(f):
@@ -30,13 +34,15 @@ def auth_required(f):
 
         try:
             payload = parse_token(request)
-        except jwt.DecodeError:
+        except jwt.DecodeError as e:
             response = jsonify(message='Token is invalid.')
             response.status_code = 401
+            app.logger.error(e)
             return response
-        except jwt.ExpiredSignature:
+        except jwt.ExpiredSignature as e:
             response = jsonify(message='Token has expired.')
             response.status_code = 401
+            app.logger.info(e)
             return response
 
         g.user_id = payload['sub']
