fa46cacb47d745498dc81bef393028f9ec6e16af
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-28 12:49:02 -0400

    rearranged to make more logical sense

--- tessera/__init__.py
+++ tessera/__init__.py
@@ -9,11 +9,11 @@ import config
 app = Flask(__name__)
 app.config.from_object(config)
 
+# Setup the log file.
 fh = logging.FileHandler(config.LOG_FILE)
-
 app.logger.addHandler(fh)
 
-db = SQLAlchemy(app)
+# Setup the application cache
 try:
     cache = RedisCache(host=config.REDIS_HOST, port=config.REDIS_PORT, 
                        password=None,db=0, default_timeout=300, key_prefix=None)
@@ -21,22 +21,25 @@ except:
     # If redis isn't set up default to a simple in memory cache.
     # This is bad for performance but makes testing and development a little
     # easier
-    log 
+    app.logger.info("""Failed to connect to redis, falling back to in memory
+                       cache this is not recommended. Please setup a redis DB.""")
     from werkzeug.contrib.cache import SimpleCache
     cache = SimpleCache()
 
-# Error handlers
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify(message="This is not the JSON you're looking for. *FORCE SOUNDS*")
+# Create the database
+db = SQLAlchemy(app)
+
+import tessera.lib.models
+db.create_all()
 
 # Register blueprints
 from tessera.api import API
 app.register_blueprint(API)
 
-# Create the database
-import tessera.lib.models
-db.create_all()
+# Error handlers
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify(message="This is not the JSON you're looking for. *FORCE SOUNDS*")
 
 # DEBUG purposes TODO: Remove
 for rule in app.url_map.iter_rules():
