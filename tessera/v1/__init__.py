de5d8d09a764b670885cf51b635d30a4772093b2
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-29 14:23:17 -0400

    renamed to use api versioning

--- tessera/v1/__init__.py
+++ tessera/v1/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-API = Blueprint("api", __name__, url_prefix='/api')
+v1 = Blueprint("v1", __name__, url_prefix='/api/v1')
 
 import tessera.api.users
 import tessera.api.sessions
