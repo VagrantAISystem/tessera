2d0703256ef148d988336a393faa6dbacd621a56
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-29 16:48:57 -0400

    updated imports

--- tessera/v1/__init__.py
+++ tessera/v1/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 
 v1 = Blueprint("v1", __name__, url_prefix='/api/v1')
 
-import tessera.api.users
-import tessera.api.sessions
+import tessera.v1.users
+import tessera.v1.sessions
