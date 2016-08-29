b20d9e2c92e7671815477edeb4eff5271997f38e
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-29 16:48:13 -0400

    added an error handler for AppError and ISE

--- tessera/errors.py
+++ tessera/errors.py
@@ -1,5 +1,7 @@
+from flask import jsonify
 from schema import SchemaError
 from tessera import app
+from tessera.lib import AppError
 
 # Error handlers
 @app.errorhandler(404)
@@ -8,8 +10,21 @@ def not_found(error):
     r.status_code = 404
     return r
 
+@app.errorhandler(500)
+def ise(error):
+    r = jsonify(message="Unexpected error.")
+    app.logger.error(str(error))
+    r.status_code = 500
+    return r
+
 @app.errorhandler(SchemaError)
 def validation_error(error):
     r = jsonify(message="Malformed JSON")
     r.status_code = 400
+    return r
+
+@app.errorhandler(AppError)
+def app_error(e):
+    r = jsonify(message=e.message)
+    r.status_code = e.code
     return r
