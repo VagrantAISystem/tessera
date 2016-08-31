35e30d1cbef164feb15609f5f8d6cce6cb7ff0a4
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-31 14:27:56 -0400

    started on ticket routes

---
+++ tessera/v1/tickets.py
@@ -0,0 +1,25 @@
+from flask import jsonify, request, g
+from tessera.v1 import v1
+from tessera.v1.models import Ticket
+
+@v1.route("/tickets", methods=["GET"])
+def ticket_index_all():
+    return jsonify([ t.to_json() for t in Team.query.all() ])
+
+@v1.route("/<string:team_slug>/<string:pkey>/tickets", methods=["GET"])
+def ticket_index(team, pkey):
+    p = Project.\
+            query.\
+            join(Project.team).\
+            join(Project.tickets).\
+            filter(Project.pkey == pkey).\
+            filter(Team.url_slug == team_slug).\
+            first()
+    return jsonify([ tk.to_json() for tk in p.tickets ])
+
+@v1.route("/<string:team_slug>/<string:pkey>/<string:ticket_key>", methods=["GET"])
+def ticket_get(ticket_key):
+    tk = Ticket.query.filter_by(ticket_key=ticket_key).first()
+    if tk == None:
+        raise AppError(status_code=404, message="Ticket not found.")
+    return jsonify( tk.to_json() )
