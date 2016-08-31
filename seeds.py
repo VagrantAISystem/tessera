4faa48dcba82e1c4f9a835150b9b7eea238c97bb
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-08-31 15:47:29 -0400

    added some debugging, not sure why team_lead isn't getting set

--- seeds.py
+++ seeds.py
@@ -15,22 +15,32 @@ db.session.add(testadmin)
 db.session.add(test)
 db.session.commit()
 
+print(test.__dict__)
+
 a_team = Team(name="The A Team")
-a_team.team_lead = testadmin
+print(a_team.__dict__)
+a_team.team_lead = test
+db.session.add(test)
 db.session.add(a_team)
 db.session.commit()
+print(a_team.__dict__)
 
-testp = Project("TEST", "Test Project")
+testp = Project(pkey="TEST", name="Test Project")
+print(testp.__dict__)
 testp.project_lead = testadmin
 a_team.projects.append(testp)
+print(testp.__dict__)
 
 for i in range(100):
-    t = Ticket(testp.pkey + "-" +
+    t = Ticket(ticket_key=testp.pkey + "-" +
                str(Project.query.filter_by(pkey=testp.pkey).first().tickets.count() + 1),
-        "This is test ticket #" + str(i + 1), "This isn't helplful")
+               summary="This is test ticket #" + str(i + 1), 
+               description="This isn't helplful")
 
     testp.tickets.append(t)
 
 db.session.add(a_team)
 db.session.add(testp)
 db.session.commit()
+
+print(testp.__dict__)
