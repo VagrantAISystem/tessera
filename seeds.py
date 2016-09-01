813a4c7b465520eb3f497fd552988c63cf31eb2f
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-01 14:22:30 -0400

    cleaned up seeds, fixed issue with team_lead

--- seeds.py
+++ seeds.py
@@ -1,7 +1,6 @@
+import sys
 from tessera import db
 from tessera.v1.models import *
-
-db.create_all()
 
 testadmin = User(username="testadmin", 
                  email="test@example.com", 
@@ -15,21 +14,14 @@ db.session.add(testadmin)
 db.session.add(test)
 db.session.commit()
 
-print(test.__dict__)
-
 a_team = Team(name="The A Team")
-print(a_team.__dict__)
 a_team.team_lead = test
-db.session.add(test)
 db.session.add(a_team)
 db.session.commit()
-print(a_team.__dict__)
 
 testp = Project(pkey="TEST", name="Test Project")
-print(testp.__dict__)
 testp.project_lead = testadmin
 a_team.projects.append(testp)
-print(testp.__dict__)
 
 for i in range(100):
     t = Ticket(ticket_key=testp.pkey + "-" +
@@ -42,5 +34,3 @@ for i in range(100):
 db.session.add(a_team)
 db.session.add(testp)
 db.session.commit()
-
-print(testp.__dict__)
