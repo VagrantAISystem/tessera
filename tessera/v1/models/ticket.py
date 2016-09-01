772d724b79565714943557ca1647a8afa7eb4dce
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-01 19:48:09 -0400

    added status to ticket, it's a bad placeholder now

--- tessera/v1/models/ticket.py
+++ tessera/v1/models/ticket.py
@@ -6,19 +6,17 @@ class Ticket(Base):
     ticket_key  = db.Column(db.String(100), nullable=False, unique=True) # I mean jesus christ how many digits
     summary     = db.Column(db.String(250), nullable=False)
     description = db.Column(db.Text())
+    status      = db.Column(db.String(10), nullable=False)
 
     assignee_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     reporter_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     project_id  = db.Column(db.Integer, db.ForeignKey('project.id'))
 
-    def __init__(self, *, ticket_key, 
-                          summary, 
-                          description, 
-                          assignee_id=None,
-                          reporter_id=None):
+    def __init__(self, *, ticket_key, summary, description, status="Open", assignee_id=None, reporter_id=None):
         self.ticket_key  = ticket_key
         self.summary     = summary
         self.description = description
+        self.status      = status
         self.assignee_id = assignee_id
         self.reporter_id = reporter_id
 
