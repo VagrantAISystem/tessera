686bbf490df3aac07ddbb5b9f0b5421fe78d944c
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-01 14:26:05 -0400

    wrote update route, changed routes to use new query function

--- tessera/v1/projects.py
+++ tessera/v1/projects.py
@@ -1,13 +1,15 @@
 from tessera import app
+from tessera.lib.tokens import auth_required
 from tessera.v1.models import Team
 from tessera.lib import AppError
-from flask import jsonify, g
+from flask import jsonify, g, request
 from tessera.v1 import v1
 
 @v1.route("/projects", methods=["GET"])
 def project_index_all():
     return jsonify([ p.to_json() for p in Project.query.all() ])
 
+# TODO: Should the query here be pulled into the Team model?
 @v1.route("/<string:team_slug>/projects", methods=["GET"])
 def project_index(team_slug):
     t = Team.query.\
@@ -20,25 +22,24 @@ def project_index(team_slug):
 
 @v1.route("/<string:team_slug>/<string:pkey>", methods=["GET"])
 def project_get(team_slug, pkey):
-    p = Project.query.\
-            join(Project.team).\
-            filter(Team.url_slug == team_slug).\
-            filter(Project.pkey == pkey).\
-            first()
-    if p == None:
-        raise AppError(status_code=404,
-                       message="That project does not exist.")
+    p = Project.get_by_key(team_slug, pkey)
     return jsonify( p.to_json() )
+
+@v1.route("/<string:team_slug>/<string:pkey>", methods=["PUT"])
+@auth_required
+def project_update(team_slug, pkey):
+    p = Project.get_by_key(team_slug, pkey)
+    if g.user.id == p.project_lead_id or g.user.is_admin:
+        p.update(request.get_json())
+        db.session.add(p)
+        db.session.commit()
+        return jsonify(message="Project successfully updated.")
+    raise AppError(status_code=403, 
+                   message="You are not permitted to perform that action")
 
 @v1.route("/<string:team_slug>/<string:pkey>/members", methods=["GET"])
 def project_members_index(team_slug, pkey):
-    m = Membership.query.\
-            join(Membership.project).\
-            join(Membership.user).\
-            join(Project.team).\
-            filter(Team.url_slug == url_slug).\
-            filter(Project.pkey == pkey).\
-            all()
-    if m == None:
-        raise AppError(status_code=404, message="Project or team not found.")
+    m = Membership.get_project_memberships(team_slug, pkey)
     return m
+
+
