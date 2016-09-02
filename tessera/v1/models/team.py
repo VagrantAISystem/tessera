34278dcc4590e29faf900999b710ce2f33cfda90
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-02 15:55:34 -0400

    fixed quotes

--- tessera/v1/models/team.py
+++ tessera/v1/models/team.py
@@ -1,5 +1,6 @@
 from tessera import db
 from sqlalchemy import or_
+from sqlalchemy.orm import joinedload
 from jsonschema import validate
 from tessera.v1.models.base import Base
 
@@ -19,59 +20,59 @@ class Team(Base):
     projects = db.relationship('Project', backref='team', lazy='dynamic')
     members  = db.relationship('Membership', backref='team', lazy='dynamic')
     
-    def __init__(self, *, name, icon=""):
+    def __init__(self, *, name, icon=''):
         self.name     = name
-        self.url_slug = name.lower().replace(" ", "-")
+        self.url_slug = name.lower().replace(' ', '-')
         self.icon          = icon
 
     def set_name(self, name):
         self.name = name
-        self.url_slug = name.lower().replace(" ", "-")
+        self.url_slug = name.lower().replace(' ', '-')
 
     def from_json(json):
         validate(json, team_schema)
-        t = Team(name=json["name"],
-                 icone=json.get("icon", ""))
-        un = json.get("project_lead",{}).get("username", "")
+        t = Team(name=json['name'],
+                 icone=json.get('icon', ''))
+        un = json.get('project_lead',{}).get('username', '')
         lead =  User.query.filter_by(username=un).first()
         t.team_lead = lead
         return t
+
+    def to_json(self):
+        s = super().to_json()
+        s.pop('team_lead_id', None)
+        s.pop('updatedDate', None)
+        return s
 
     def get_by_name_or_stub(name):
-        t = Team.query.filter(or_(Team.name == name, 
-                                  Team.url_stub == name)).first()
+        t = Team.query.\
+                options(joinedload(Team.team_lead)).\
+                filter(or_(Team.name == name, 
+                           Team.url_slug == name)).first()
+        print(t.team_lead)
         if t == None:
             raise AppError(status_code=404,
-                           message="That team does not exist")
-        return t
-
-    def from_json(json):
-        validate(json, team_schema)
-        t = Team(name=json["name"],
-                 icone=json.get("icon", ""))
-        un = json.get("project_lead",{}).get("username", "")
-        lead =  User.query.filter_by(username=un).first()
-        t.team_lead = lead
+                           message='That team does not exist')
         return t
 
     def __repr__(self):
-        return "<Team %r>" % (self.name)
+        return '<Team %r>' % (self.name)
 
 team_schema = {
-    "type": "object",
-    "properties": {  
-        "name": { "type": "string" },
-        "icon": { "type": "string" },
-        "team_lead": { 
-            "type": "object",
-            "properties": {
-                "id": { "type": "integer" },
-                "username": { "type": "string" },
-                "email": { "type": "string" },
-                "fullName": { "type": "string" },
+    'type': 'object',
+    'properties': {  
+        'name': { 'type': 'string' },
+        'icon': { 'type': 'string' },
+        'team_lead': { 
+            'type': 'object',
+            'properties': {
+                'id': { 'type': 'integer' },
+                'username': { 'type': 'string' },
+                'email': { 'type': 'string' },
+                'fullName': { 'type': 'string' },
             },
-            "required": [ "username" ],
+            'required': [ 'username' ],
         },
     },
-    "required": [ "team_lead", "name" ],
+    'required': [ 'team_lead', 'name' ],
 }
