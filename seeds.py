e494b3214990cadc78a37037ca7904ecc281b689
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-02 17:57:41 -0400

    added statuses to seeds.py

--- seeds.py
+++ seeds.py
@@ -20,10 +20,34 @@ a_team.team_lead = test
 db.session.add(a_team)
 db.session.commit()
 
+# make the default statuses
+backlog     = Status(name="Backlog", status_type=0)
+in_progress = Status(name="In Progress", status_type=1)
+on_hold     = Status(name="On Hold", status_type=1)
+closed      = Status(name="Closed", status_type=2)
+
+backlog.next_statuses.append(in_progress)
+backlog.next_statuses.append(on_hold)
+in_progress.next_statuses.append(closed)
+in_progress.next_statuses.append(on_hold)
+on_hold.next_statuses.append(backlog)
+on_hold.next_statuses.append(in_progress)
+on_hold.next_statuses.append(closed)
+closed.next_statuses.append(backlog)
+
+db.session.add(backlog)
+db.session.add(in_progress)
+db.session.add(on_hold)
+db.session.add(closed)
+db.session.commit()
+
+statuses = [ backlog, in_progress, on_hold, closed ]
+
 testp = Project(pkey="TEST", name="Test Project")
 testp.project_lead = testadmin
 a_team.projects.append(testp)
 
+s = 0
 for i in range(100):
     t = Ticket(ticket_key=testp.pkey + "-" +
                str(Project.query.filter_by(pkey=testp.pkey).first().tickets.count() + 1),
@@ -31,7 +55,10 @@ for i in range(100):
                description="This isn't helplful")
     t.assignee = test
     t.reporter = testadmin
-
+    t.status   = statuses[s]
+    s += 1
+    if s == len(statuses):
+        s = 0
     testp.tickets.append(t)
 
 db.session.add(a_team)
