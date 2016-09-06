e1591b4cece10357e2c6273ae701ff69e9ed3036
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-06 19:56:22 -0400

    added comment model

--- tessera/v1/models/__init__.py
+++ tessera/v1/models/__init__.py
@@ -4,4 +4,5 @@ from tessera.v1.models.ticket import Ticket
 from tessera.v1.models.project import Project
 from tessera.v1.models.team import Team
 from tessera.v1.models.user import User
+from tessera.v1.models.comment import Comment
 from tessera.v1.models.membership import Membership
