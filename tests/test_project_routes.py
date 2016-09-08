24d4e1748ecd00de12b163f47012a4d233be9ae3
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-08 16:07:36 -0400

    stubbed out project tests

---
+++ tests/test_project_routes.py
@@ -0,0 +1,19 @@
+import tests
+import json
+from tessera.v1.models.schemas import project_schema
+
+# Create
+def test_project_create():
+    assert False
+
+# Read
+def test_project_get():
+    assert False
+
+# Update
+def test_project_update():
+    assert False
+
+# Delete
+def test_project_delete():
+    assert False
