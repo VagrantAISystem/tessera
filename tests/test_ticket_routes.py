c5eec25d08ec04a4552aafdaac848fa2d5ad91e3
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-08 15:54:24 -0400

    finished up ticket CRUD route tests
    still need to write tests for statuses and custom fields

--- tests/test_ticket_routes.py
+++ tests/test_ticket_routes.py
@@ -4,21 +4,29 @@ from tessera.v1.models.schemas import ticket_test_schema
 
 # Create
 def test_ticket_create():
+    r = tests.t_post("/api/v1/the-a-team/TEST/tickets",
+                     { 
+                         "summary": "This is a unit test ticket.",
+                         "description": "I hope this unit test passes.",
+                         "reporter": {
+                             "username": "test",
+                             "email": "test1@example.com",
+                             "fullName": "Test Testerson",
+                         },
+                     })
+    assert r.status_code == 401
     r = tests.a_post("/api/v1/the-a-team/TEST/tickets",
-                    { 
-                        "summary": "This is a unit test ticket.",
-                        "description": "I hope this unit test passes.",
-                        "reporter": {
-                            "username": "test",
-                            "email": "test1@example.com",
-                            "fullName": "Test Testerson",
-                        },
-                    })
+                     { 
+                         "summary": "This is a unit test ticket.",
+                         "description": "I hope this unit test passes.",
+                         "reporter": {
+                             "username": "test",
+                             "email": "test1@example.com",
+                             "fullName": "Test Testerson",
+                         },
+                     })
     jsn = json.loads(r.data.decode("utf-8"))
-    assert jsn == { 
-                    "message": "Ticket successfully created.", 
-                    "link": "/api/v1/the-a-team/TEST/TEST-101"
-                  }
+    assert tests.test_json(jsn, ticket_test_schema)
 
 # Read
 def test_ticket_get():
@@ -27,4 +35,38 @@ def test_ticket_get():
     assert tests.test_json(jsn, ticket_test_schema)
 
 # Update
+def test_ticket_update():
+    r = tests.t_put("/api/v1/the-a-team/TEST/TEST-1", 
+                    {
+                        "summary": "This is not a unit test ticket.",
+                        "description": "I hope this unit test passes.",
+                        "reporter": {
+                            "username": "testadmin",
+                        },
+                    })
+    assert r.status_code == 401
+    r = tests.a_put("/api/v1/the-a-team/TEST/TEST-1", 
+                    {
+                        "summary": "This is not a unit test ticket.",
+                        "description": "I hope this unit test passes.",
+                        "reporter": {
+                            "username": "testadmin",
+                        },
+                    })
+    jsn = json.loads(r.data.decode("utf-8"))
+    assert jsn == {
+        "summary": "This is not a unit test ticket.",
+        "description": "I hope this unit test passes.",
+        "reporter": {
+            "username": "testadmin",
+        },
+    }
+
 # Delete
+def test_ticket_delete():
+    r = tests.a_delete("/api/v1/the-a-team/TEST/TEST-2")
+    assert r.status_code == 401
+    r = tests.a_delete("/api/v1/the-a-team/TEST/TEST-2")
+    assert r.status_code == 200
+    r = tests.a_get("/api/v1/the-a-team/TEST/TEST-1")
+    assert r.status_code == 404
