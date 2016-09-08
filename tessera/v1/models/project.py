e1920d5a36efdbb4c551268146509881e10ac761
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-08 10:04:21 -0400

    changed lazy mode for project tickets relationship

--- tessera/v1/models/project.py
+++ tessera/v1/models/project.py
@@ -14,7 +14,7 @@ class Project(Base):
     project_lead_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     team_id         = db.Column(db.Integer, db.ForeignKey('team.id'))
 
-    tickets = db.relationship('Ticket', backref='project', lazy='dynamic')
+    tickets = db.relationship('Ticket', backref='project')
     members = db.relationship('Membership', backref='project', lazy='dynamic')
 
     def __init__(self, *, pkey, name, repo='', homepage=''):
@@ -33,17 +33,6 @@ class Project(Base):
         if p == None:
             raise AppError(status_code=404, message="Project not found.")
         return p
-
-    def get_tickets(self):
-        p = Project.query.\
-                options(joinedload(Project.tickets)).\
-                join(Project.team).\
-                filter(Project.pkey == pkey).\
-                filter(Team.url_slug == team_slug).\
-                first()
-        if p == None:
-            raise AppError(status_code=404, message="Project not found.")
-        return p.tickets
 
     def to_json(self):
         s = super().to_json()
