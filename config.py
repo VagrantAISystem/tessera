4d181b84a6530ed657448d0d1544884225bf27cf
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-09 11:09:48 -0400

    productionalized the configuration file

--- config.py
+++ config.py
@@ -1,19 +1,37 @@
+import random
+import string
+import os
+
 DEBUG = True
 
+TESSERA_ENV = os.environ.get("TESSERA_ENV", "development")
+
+if TESSERA_ENV == "production":
+    DEBUG = False
+
 # Define the application directory
-import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Database configuration
-# TODO: Change this to grab the environment variable and use a real DB
-TEST_DB_LOCATION = os.path.join(BASE_DIR, 'tessera.db')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + TEST_DB_LOCATION
+DEFAULT_DB_LOCATION = os.path.join(BASE_DIR, 'tessera.db')
+SQLALCHEMY_DATABASE_URI = os.environ.get("TESSERA_DB_URL", 
+                                         'sqlite:///' + DEFAULT_DB_LOCATION)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DATABASE_CONNECT_OPTIONS = {}
 
-SECRET_KEY = "loremipsumisnotsecure"
+SECRET_KEY_FILE = os.path.abspath(os.path.join(BASE_DIR, ".tessera_secret_key"))
+if os.path.isfile(SECRET_KEY_FILE):
+    with open(SECRET_KEY_FILE) as skf:
+        SECRET_KEY = skf.read()
+else:
+    SECRET_KEY = ''.join(random.SystemRandom().choice(string.ascii_uppercase +
+                                         string.digits) for _ in range(N))
+    with open(SECRET_KEY_FILE, "w") as skf:
+        skf.write(SECRET_KEY)
 
 LOG_FILE = os.path.abspath(os.path.join(BASE_DIR, "logs", "tessera.log"))
+if not os.path.isdir(os.path.abspath(os.path.join(BASE_DIR, "logs"))):
+   os.mkdir(os.path.abspath(os.path.join(BASE_DIR, "logs"))) 
 
-REDIS_HOST = "localhost"
-REDIS_PORT = 6379
+REDIS_HOST = os.environ.get("TESSERA_REDIS_HOST", "localhost")
+REDIS_PORT = int(os.environ.get("TESSERA_REDIS_PORT", "6379"))
