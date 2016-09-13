55b0b2e12964f57e9cabfaefdb86c282368abf8f
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-13 18:32:15 -0400

    added auth_required to ticket routes

--- tessera/lib/__init__.py
+++ tessera/lib/__init__.py
@@ -8,17 +8,6 @@ from functools import wraps
 from flask import session
 from flask.json import jsonify
 
-def requires_login(f):
-    """This decorator protects a route behind authentication."""
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session['user_id'] is None:
-            err = jsonify({ "error_message": "Not authorized." })
-            err.status_code = 403
-            return err
-        return f(*args, **kwargs)
-    return decorated_function
-
 class AppError(Exception):
     def __init__(self, *, status_code, message):
         self.message = message
