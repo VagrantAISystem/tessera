26431b11e914fb472e136037a8f47142407ad811
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-13 21:37:33 -0400

    updated to point at new routes

--- tests/test_ticket_routes.py
+++ tests/test_ticket_routes.py
@@ -4,7 +4,7 @@ from tessera.models.v1.schemas import ticket_test_schema
 
 # Create
 def test_ticket_create():
-    r = tests.t_post("/api/v1/the-a-team/TEST/tickets",
+    r = tests.t_post("/api/v1/teams/the-a-team/TEST/tickets",
                      { 
                          "summary": "This is a unit test ticket.",
                          "description": "I hope this unit test passes.",
@@ -15,7 +15,7 @@ def test_ticket_create():
                          },
                      })
     assert r.status_code == 401
-    r = tests.a_post("/api/v1/the-a-team/TEST/tickets",
+    r = tests.a_post("/api/v1/teams/the-a-team/TEST/tickets",
                      { 
                          "summary": "This is a unit test ticket.",
                          "description": "I hope this unit test passes.",
@@ -30,13 +30,13 @@ def test_ticket_create():
 
 # Read
 def test_ticket_get():
-    r = tests.a_get("/api/v1/the-a-team/TEST/TEST-1")
+    r = tests.a_get("/api/v1/teams/the-a-team/TEST/TEST-1")
     jsn = json.loads(r.data.decode("utf-8"))
     assert tests.test_json(jsn, ticket_test_schema)
 
 # Update
 def test_ticket_update():
-    r = tests.t_put("/api/v1/the-a-team/TEST/TEST-1", 
+    r = tests.t_put("/api/v1/teams/the-a-team/TEST/TEST-1", 
                     {
                         "summary": "This is not a unit test ticket.",
                         "description": "I hope this unit test passes.",
@@ -45,7 +45,7 @@ def test_ticket_update():
                         },
                     })
     assert r.status_code == 401
-    r = tests.a_put("/api/v1/the-a-team/TEST/TEST-1", 
+    r = tests.a_put("/api/v1/teams/the-a-team/TEST/TEST-1", 
                     {
                         "summary": "This is not a unit test ticket.",
                         "description": "I hope this unit test passes.",
@@ -64,9 +64,9 @@ def test_ticket_update():
 
 # Delete
 def test_ticket_delete():
-    r = tests.a_delete("/api/v1/the-a-team/TEST/TEST-2")
+    r = tests.a_delete("/api/v1/teams/the-a-team/TEST/TEST-2")
     assert r.status_code == 401
-    r = tests.a_delete("/api/v1/the-a-team/TEST/TEST-2")
+    r = tests.a_delete("/api/v1/teams/the-a-team/TEST/TEST-2")
     assert r.status_code == 200
-    r = tests.a_get("/api/v1/the-a-team/TEST/TEST-1")
+    r = tests.a_get("/api/v1/teams/the-a-team/TEST/TEST-1")
     assert r.status_code == 404
