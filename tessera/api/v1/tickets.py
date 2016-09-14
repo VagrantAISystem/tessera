27ffc23f5c830f33f7d21e6be66c195444b6de2a
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-14 16:46:48 -0400

    updated routes to match new scheme

--- tessera/api/v1/tickets.py
+++ tessera/api/v1/tickets.py
@@ -7,12 +7,12 @@ from tessera.models.v1 import Ticket, Project, Comment
 def ticket_index_all():
     return jsonify([ t.to_json() for t in Ticket.query.all() ])
 
-@v1.route("/teams/<string:team_slug>/<string:pkey>/tickets", methods=["GET"])
+@v1.route("/tickets/<string:team_slug>/<string:pkey>/tickets", methods=["GET"])
 def ticket_index(team_slug, pkey):
     p = Project.get_tickets(team_slug, pkey)
     return jsonify([ tk.to_json() for tk in p.tickets ])
 
-@v1.route("/teams/<string:team_slug>/<string:pkey>/tickets", methods=["POST"])
+@v1.route("/tickets/<string:team_slug>/<string:pkey>/tickets", methods=["POST"])
 def ticket_create(team_slug, pkey):
     jsn = request.get_json()
     p = Project.get_by_key(team_slug, pkey)
@@ -21,34 +21,34 @@ def ticket_create(team_slug, pkey):
     db.session.commit()
     return jsonify(tk.to_json())
 
-@v1.route("/teams/<string:team_slug>/<string:pkey>/<string:ticket_key>", methods=["GET"])
+@v1.route("/tickets/<string:team_slug>/<string:pkey>/<string:ticket_key>", methods=["GET"])
 def ticket_get(team_slug, pkey, ticket_key):
     prl = request.args.get("preload", "")
     tk = Ticket.get_by_key(team_slug, pkey, ticket_key, prl) 
     return jsonify( tk.to_json() )
 
-@v1.route("/teams/<string:team_slug>/<string:pkey>/<string:ticket_key>", methods=["PUT"])
+@v1.route("/tickets/<string:team_slug>/<string:pkey>/<string:ticket_key>", methods=["PUT"])
 def ticket_update(team_slug, pkey, ticket_key):
     return jsonify(message="Not implemented")
 
-@v1.route("/teams/<string:team_slug>/<string:pkey>/<string:ticket_key>", methods=["DELETE"])
+@v1.route("/tickets/<string:team_slug>/<string:pkey>/<string:ticket_key>", methods=["DELETE"])
 def ticket_delete(team_slug, pkey, ticket_key):
     return jsonify(message="Not implemented")
 
-@v1.route("/teams/<string:team_slug>/<string:pkey>/<string:ticket_key>/next", methods=["GET"])
+@v1.route("/tickets/<string:team_slug>/<string:pkey>/<string:ticket_key>/next", methods=["GET"])
 def ticket_next(team_slug, pkey, ticket_key):
     return jsonify(message="Not implemented")
 
-@v1.route("/teams/<string:team_slug>/<string:pkey>/<string:ticket_key>/prev", methods=["GET"])
+@v1.route("/tickets/<string:team_slug>/<string:pkey>/<string:ticket_key>/prev", methods=["GET"])
 def ticket_prev(team_slug, pkey, ticket_key):
     return jsonify(message="Not implemented")
 
-@v1.route("/teams/<string:team_slug>/<string:pkey>/<string:ticket_key>/comments", methods=["GET"])
+@v1.route("/tickets/<string:team_slug>/<string:pkey>/<string:ticket_key>/comments", methods=["GET"])
 def comment_index(team_slug, pkey, ticket_key):
     cmts = Comment.get_all(team_slug, pkey, ticket_key)
     return jsonify([ c.to_json() for c in cmts ])
 
-@v1.route("/teams/<string:team_slug>/<string:pkey>/<string:ticket_key>/comments", methods=["POST"])
+@v1.route("/tickets/<string:team_slug>/<string:pkey>/<string:ticket_key>/comments", methods=["POST"])
 def comment_create(team_slug, pkey, ticket_key):
     j = request.get_json()
     c = Comment.from_json(j)
