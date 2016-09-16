177f3a1e8c154a3700f6418abfdc0dae6994e97d
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-15 21:47:05 -0400

    made the pool size conditional
    since sqlite doesn't support it

--- config.py
+++ config.py
@@ -11,9 +11,12 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DEFAULT_DB_LOCATION = os.path.join(BASE_DIR, 'tessera.db')
 SQLALCHEMY_DATABASE_URI = os.environ.get("TESSERA_DB_URL", 
                                          'sqlite:///' + DEFAULT_DB_LOCATION)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_POOL_SIZE = 10
 DATABASE_CONNECT_OPTIONS = {}
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+if "postgres" in SQLALCHEMY_DATABASE_URI:
+    SQLALCHEMY_POOL_SIZE = 10
+
 
 SECRET_KEY_FILE = os.path.abspath(os.path.join(BASE_DIR, ".tessera_secret_key"))
 if os.path.isfile(SECRET_KEY_FILE):
