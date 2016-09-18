2e1560c829de6dd3b2145d93c1e07c79c3e2cefb
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-18 12:20:08 -0400

    added services to travis

--- config.py
+++ config.py
@@ -9,14 +9,13 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Database configuration
 DEFAULT_DB_LOCATION = os.path.join(BASE_DIR, 'tessera.db')
-SQLALCHEMY_DATABASE_URI = os.environ.get("TESSERA_DB_URL", 
+SQLALCHEMY_DATABASE_URI = os.environ.get("TESSERA_DB_URL",
                                          'sqlite:///' + DEFAULT_DB_LOCATION)
 DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 if "postgres" in SQLALCHEMY_DATABASE_URI:
     SQLALCHEMY_POOL_SIZE = 10
-
 
 SECRET_KEY_FILE = os.path.abspath(os.path.join(BASE_DIR, ".tessera_secret_key"))
 if os.path.isfile(SECRET_KEY_FILE):
@@ -30,7 +29,7 @@ else:
 
 LOG_FILE = os.path.abspath(os.path.join(BASE_DIR, "logs", "tessera.log"))
 if not os.path.isdir(os.path.abspath(os.path.join(BASE_DIR, "logs"))):
-   os.mkdir(os.path.abspath(os.path.join(BASE_DIR, "logs"))) 
+   os.mkdir(os.path.abspath(os.path.join(BASE_DIR, "logs")))
 
 REDIS_HOST = os.environ.get("TESSERA_REDIS_HOST", "localhost")
 REDIS_PORT = int(os.environ.get("TESSERA_REDIS_PORT", "6379"))
