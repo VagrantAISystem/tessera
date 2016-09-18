6d63732dcd5feffc3aebf609efe2644b51c554ff
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-17 22:54:34 -0400

    finished field route integration testing

--- tests/integration_tests/test_field_routes.py
+++ tests/integration_tests/test_field_routes.py
@@ -1,19 +1,46 @@
 import tests
-import json
-# from tessera.models.v1.schemas import field_schema
 
-# Create
-def test_field_create():
-    assert False
+class TestFieldRoutes():
+    # Create
+    def test_field_create(self):
+        r = tests.t_post("/api/v1/fields",
+                             {
+                                 "name": "Unit Test Field",
+                                 "data_type": "STRING",
+                             })
+        assert r.status_code == 401
+        r = tests.a_post("/api/v1/fields",
+                             {
+                                 "name": "Unit Test Field",
+                                 "data_type": "STRING",
+                             })
+        assert r.status_code == 200
 
-# Read
-def test_field_get():
-    assert False
+    # Read
+    def test_field_get(self):
+        r = tests.t_get("/api/v1/fields/0")
+        assert r.status_code == 404
+        r = tests.a_get("/api/v1/fields/0")
+        assert r.status_code == 200
 
-# Update
-def test_field_update():
-    assert False
+    # Update
+    def test_field_update(self):
+        r = tests.t_put("/api/v1/fields/0",
+                        {
+                            "name": "Unit Test Update",
+                        })
+        assert r.status_code == 401
+        r = tests.t_put("/api/v1/fields/0",
+                        {
+                            "name": "Unit Test Update",
+                        })
+        assert r.status_code == 200
 
-# Delete
-def test_field_delete():
-    assert False
+    # Delete
+    def test_field_delete(self):
+        r = tests.t_delete("/api/v1/fields/1")
+        assert r.status_code == 401
+        r = tests.a_delete("/api/v1/fields/1")
+        assert r.status_code == 200
+        r = tests.a_get("/api/v1/fields/1")
+        assert r.status_code == 404
