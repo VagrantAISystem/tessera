1add8bfbfebf326183202103bd0ceaf91edb4c5e
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-17 22:54:23 -0400

    added a setup_module function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,8 @@
-import pytest
 import os
 import config
 
-@pytest.fixture(scope="module")
-def db(request):
-    """Clears out the database and re seeds it for the tests.
-    then removes the database after the tests."""
+def setup_module():
+    """Creates a test database and re seeds it for the tests. Will clean up after itself"""
     if os.path.isfile(config.DEFAULT_DB_LOCATION):
         os.remove(config.DEFAULT_DB_LOCATION)
     import seeds
-    yield None
-    if os.path.isfile(config.DEFAULT_DB_LOCATION):
-        os.remove(config.DEFAULT_DB_LOCATION)
