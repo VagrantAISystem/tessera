0409e330179cd92f221e3a3b8277c41deac57d55
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-19 18:20:46 -0400

    added __tablename__ cleand up set_value

--- tessera/models/v1/field.py
+++ tessera/models/v1/field.py
@@ -20,31 +20,37 @@ project_field_schema = db.Table(
 )
 
 class Field(Base):
+    __tablename__ = "fields"
+
     name      = db.Column(db.String(100), nullable=False)
     data_type = db.Column(db.Enum("INTEGER", "FLOAT", "STRING", "TEXT",
                                   name="data_types"), nullable=False)
 
 class FieldValue(Base):
+    __tablename__ = "field_values"
+
     field_id      = db.Column('field_id', db.Integer,
                               db.ForeignKey('field.id'), nullable=False)
     ticket_id     = db.Column('ticket_id', db.Integer,
                               db.ForeignKey('ticket.id'), nullable=False)
-    text_value    = db.Column(db.Text())
-    string_value  = db.Column(db.String(250))
-    float_value   = db.Column(db.Float)
-    integer_value = db.Column(db.Integer)
+
+    text_value    = db.Column('text_value', db.Text())
+    string_value  = db.Column('string_value', db.String(250))
+    float_value   = db.Column('float_value', db.Float)
+    integer_value = db.Column('integer_value', db.Integer)
 
     def validate_value(self):
         if (
             (self.field.data_type == DataTypes.INTEGER and type(self.value) is not int) or
             (self.field.data_type == DataTypes.FLOAT and type(self.value) is not float) or
             (self.field.data_type == DataTypes.TEXT and type(self.value) is not str) or
-            (self.field.data_type == DataTypes.STRING and type(self.value) is not str)
+            (self.field.data_type == DataTypes.STRING and type(self.value) is not str) or
+            (self.value == None)
            ):
-            raise AppError(status_code=400, message='Invalid type for the field: ' + self.name)
+            return False
+        return True
 
     def set_value(self):
-        self.validate_value()
         if self.field.data_type == DataTypes.INTEGER:
             self.integer_value = self.value
         elif self.field.data_type == DataTypes.FLOAT:
@@ -54,8 +60,8 @@ class FieldValue(Base):
         elif self.field.data_type == DataTypes.STRING:
             self.string_value = self.value
         else:
-            raise AppError(status_code=500,
-                           message='Uknown error setting field value')
+            self.value = None
+        return self.validate_value()
 
     def from_json(jsn):
         parent_field = Field.query.filter_by(name=jsn.get("name", ""))
