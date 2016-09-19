6116af453a111d2ce68c950e7ef6a7b4f86443dc
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-19 19:53:47 -0400

    removed non-working methods

--- tessera/models/v1/status.py
+++ tessera/models/v1/status.py
@@ -8,13 +8,7 @@ class Status(Base):
     __tablename__ = "statuses"
 
     name          = db.Column(db.String(100), nullable=False)
-    status_type   = db.Column(db.Enum("TODO", "IN_PROGRESS", "DONE", name='status_types'))
-
+    status_type   = db.Column(db.Enum("TODO", "IN_PROGRESS", "DONE",
+                                      name='status_types'))
     def to_json(self):
         return super().to_json(ignoreFields=["created_at", "updated_at"])
-
-    def get_next(self):
-        return self.next_statuses.all()
-
-    def get_previous(self):
-        return self.previous_statuses.all()
