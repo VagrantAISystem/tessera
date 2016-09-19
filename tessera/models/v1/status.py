7821dfb1affc58ca3e6b8f53d6b4508238a0ee6e
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-19 18:41:58 -0400

    removed __init__

--- tessera/models/v1/status.py
+++ tessera/models/v1/status.py
@@ -9,18 +9,6 @@ class Status(Base):
 
     name          = db.Column(db.String(100), nullable=False)
     status_type   = db.Column(db.Enum("TODO", "IN_PROGRESS", "DONE", name='status_types'))
-    next_statuses = db.relationship('Status',
-                                    secondary=status_relationships,
-                                    primaryjoin="Status.id == status_relationships.c.status_id",
-                                    secondaryjoin="Status.id == status_relationships.c.next_status_id",
-                                    backref='previous_statuses',
-                                    lazy='dynamic')
-
-    tickets = db.relationship('Ticket', backref='status', lazy='dynamic')
-
-    def __init__(self, *, name, status_type=0):
-        self.name        = name
-        self.status_type = status_type
 
     def to_json(self):
         return super().to_json(ignoreFields=["created_at", "updated_at"])
